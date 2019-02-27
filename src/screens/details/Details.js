75e13715f7f29128f3e7403c44836c4be2d6e1f7
mukul <mnshrt2292@gmail.com>
2019-02-28 04:07:34 +0530

    the user information section of the profile page was added

---
+++ src/screens/details/Details.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import InfoSection from './infoSection/InfoSection'
+class Details extends Component{
+
+    render(){
+        return( 
+        <div>
+               {/*<Header/> */} 
+               <InfoSection/>
+
+
+        </div>);
+         
+        }
+        
+    
+    
+}
+export default Details;
