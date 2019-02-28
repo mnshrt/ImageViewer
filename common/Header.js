ccf9cdad081ca33daf9af99b5586db137fead39d
Karan Pillai <karanpillai93@gmail.com>
2019-02-28 19:44:29 +0530

    Updated Header.js
    Changes made to Header.js to make the header common for all pages with conditional rendering

--- common/Header.js
+++ common/Header.js
@@ -1,6 +1,6 @@
 // Imports
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+import { Redirect } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
@@ -9,46 +9,171 @@ import IconButton from "@material-ui/core/IconButton";
 import InputBase from "@material-ui/core/InputBase";
 import SearchIcon from "@material-ui/icons/Search";
 import Grid from "@material-ui/core/Grid";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
 import "./Header.css";
 
-// Component
-const Header = props => {
-  const { classes } = props;
-  return (
-    <div className="header-container">
-      <AppBar className="navbar" position="static">
-        <Toolbar>
-          <Grid
-            justify="space-between" // Add it here :)
-            container
-            style={{ alignItems: "center" }}
-            spacing={2}
-          >
-            <Typography
-              variant="h6"
-              color="inherit"
-              style={{ fontSize: "18px" }}
-            >
-              Image Viewer
-            </Typography>
-            <div className="search-bar-container">
-              <SearchIcon className="search-icon" />
-              <InputBase className="search-bar" placeholder="Search..." />
-            </div>
-            <IconButton>
-              <Avatar
-                style={{ flex: 1 }}
-                className="user-profile-avatar"
-                alt="Remy Sharp"
-                src="https://images.upgrad.com/dbcba774-1e6f-4838-b4ad-816468779a4b-Screenshot%202018-10-04%20at%205.51.45%20PM.png"
-              />
-            </IconButton>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-};
+// Header Component
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    anchorEl: null,
+    extendedHeader: false,
+    searchBar: false,
+    profile: false,
+    redirect: false
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    });
+  };
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+  };
+
+  componentDidMount() {
+    this.setState({
+      extendedHeader: this.props.extendedHeader,
+      searchBar: this.props.searchBar,
+      profile: this.props.profile
+    });
+  }
+
+  render() {
+    const postFilter = this.props.filterPostsFunction;
+    const image = this.props.image;
+    const open = Boolean(this.state.anchorEl);
+    return (
+      <div className="header-container">
+        {this.renderRedirect()}
+        {this.state.extendedHeader ? (
+          <AppBar className="navbar" position="static">
+            <Toolbar>
+              <Grid
+                justify="space-between" // Add it here :)
+                container
+                style={{ alignItems: "center" }}
+                spacing={2}
+              >
+                <Typography
+                  variant="h6"
+                  color="inherit"
+                  style={{ fontSize: "18px" }}
+                >
+                  Image Viewer
+                </Typography>
+                {this.state.searchBar ? (
+                  <div className="search-bar-container">
+                    <SearchIcon className="search-icon" />
+                    <InputBase
+                      className="search-bar"
+                      placeholder="Search..."
+                      onChange={postFilter}
+                    />
+                  </div>
+                ) : null}
+                <IconButton
+                  aria-owns={open ? "menu-appbar" : undefined}
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <Avatar
+                    style={{ flex: 1 }}
+                    className="user-profile-avatar"
+                    alt="Remy Sharp"
+                    src={image}
+                  />
+                </IconButton>
+                <div>
+                  <Menu
+                    className="my-menu"
+                    id="menu-appbar"
+                    anchorEl={this.state.anchorEl}
+                    children="menu-list"
+                    getContentAnchorEl={null}
+                    anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                    transformOrigin={{ vertical: "top", horizontal: "center" }}
+                    open={open}
+                    onClose={this.handleClose}
+                  >
+                    {!this.state.profile ? (
+                      <div className="menu-item-container">
+                        <MenuList id="menu-list">
+                          <MenuItem
+                            className="menu-item"
+                            onClick={this.handleClose}
+                          >
+                            My Account
+                          </MenuItem>
+                          <hr />
+                          <MenuItem
+                            className="menu-item"
+                            onClick={this.setRedirect}
+                          >
+                            Logout
+                          </MenuItem>
+                        </MenuList>
+                      </div>
+                    ) : (
+                      <div className="menu-item-container">
+                        <MenuList id="menu-list">
+                          <MenuItem
+                            className="menu-item"
+                            onClick={this.setRedirect}
+                          >
+                            Logout
+                          </MenuItem>
+                        </MenuList>
+                      </div>
+                    )}
+                  </Menu>
+                </div>
+              </Grid>
+            </Toolbar>
+          </AppBar>
+        ) : (
+          <AppBar className="navbar" position="static">
+            <Toolbar>
+              <Grid
+                justify="space-between" // Add it here :)
+                container
+                style={{ alignItems: "center" }}
+                spacing={2}
+              >
+                <Typography
+                  variant="h6"
+                  color="inherit"
+                  style={{ fontSize: "18px" }}
+                >
+                  Image Viewer
+                </Typography>
+              </Grid>
+            </Toolbar>
+          </AppBar>
+        )}
+      </div>
+    );
+  }
+}
 
 // Export
 export default Header;
+
