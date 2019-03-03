9b96427382b3f5952c51ed3fc50b218a8065ddc4
Karan Pillai <karanpillai93@gmail.com>
2019-03-03 23:40:01 +0530

    Updated Profile component

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,22 +1,34 @@
-import React, {Component} from 'react';
-import InfoSection from './infoSection/InfoSection'
-import ImageGridListSection from './imageGridListSection/ImageGridListSection';
-class Profile extends Component{
+import React, { Component } from "react";
+import InfoSection from "./infoSection/InfoSection";
+import ImageGridListSection from "./imageGridListSection/ImageGridListSection";
+import Header from "../../common/Header";
+import { withRouter } from "react-router-dom";
 
-    render(){
-        return( 
-        <div>
-               {/*<Header/> */} 
-               <InfoSection/>
-               <ImageGridListSection/>
-               {/*  **/}
+class Profile extends Component {
+  state = {
+    profileImage: undefined
+  };
 
+  componentDidMount() {
+    this.setState({
+      profileImage: this.props.location.state.image
+    });
+  }
 
-        </div>);
-         
-        }
-        
-    
-    
+  render() {
+    return (
+      <div>
+        {/** Here we need to provide the props to the header*/}
+        <Header
+          image={this.state.profileImage}
+          extendedHeader={true}
+          searchBar={false}
+          profile={true}
+        />
+        <InfoSection />
+        <ImageGridListSection />
+      </div>
+    );
+  }
 }
-export default Profile;
+export default withRouter(Profile);
