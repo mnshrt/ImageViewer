1ea61c911d28e4f7e7038857dd7d66c37ecab371
Karan Pillai <karanpillai93@gmail.com>
2019-03-03 20:12:32 +0530

    Updated Login screen
    Changes made to Login.js to fix imports

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,3 +1,17 @@
+/ Imports
+import React, { Component } from "react";
+import Typography from "@material-ui/core/Typography";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import PropTypes from "prop-types";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import { Redirect } from "react-router-dom";
+import "./Login.css";
+
 // Card container
 const CardContainer = function(props) {
   return (
@@ -163,4 +177,3 @@ class Login extends Component {
 
 // Export
 export default Login;
-
