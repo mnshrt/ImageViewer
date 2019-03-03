578352fc7cfd3a54280d89b74cdea77ee2834d04
Karan Pillai <karanpillai93@gmail.com>
2019-03-03 23:56:44 +0530

    Final commit

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -5,26 +5,11 @@ import Header from "../../common/Header";
 import { withRouter } from "react-router-dom";
 
 class Profile extends Component {
-  state = {
-    profileImage: undefined
-  };
-
-  componentDidMount() {
-    this.setState({
-      profileImage: this.props.location.state.image
-    });
-  }
 
   render() {
     return (
       <div>
-        {/** Here we need to provide the props to the header*/}
-        <Header
-          image={this.state.profileImage}
-          extendedHeader={true}
-          searchBar={false}
-          profile={true}
-        />
+        {/** Here we need to provide the props to the header*/
         <InfoSection />
         <ImageGridListSection />
       </div>
