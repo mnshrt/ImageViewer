a6b7f60f686b222fdeecead681c048eccec61a98
Karan Pillai <karanpillai93@gmail.com>
2019-03-03 17:53:27 +0530

    Updated routing
    Changes made to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 // Imports
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
-import Home from "./screens/Home";
-import Login from "./screens/Login";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./screens/Home/Home";
+import Login from "./screens/Login/Login";
+import Profile from "./screens/Profile/Profile";
 import "./App.css";
 
 // App component
 class App extends Component {
   render() {
     return (
-      <HashRouter>
+      <BrowserRouter>
         <div>
           {/* <Route exact path="/" component={Login} /> */}
           <Route exact path="/home" component={Home} />
           {/* <Route exact path="/profile" component={Profile} /> */}
         </div>
-      </HashRouter>
+      </BrowserRouter>
     );
   }
 }
