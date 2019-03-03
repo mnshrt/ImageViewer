c2c4cd72d75e8868ba9a903fbf74e213158b7327
Karan Pillai <karanpillai93@gmail.com>
2019-03-03 19:24:50 +0530

    Updated index.js
    Changes made to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Login from './screens/Login/Login';
+// Imports
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(
-    <Login/>, 
-    document.getElementById('root')
-);
+// Main render
+ReactDOM.render(<App />, document.getElementById("root"));
