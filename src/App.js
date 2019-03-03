0bfcf59ff7f074e778acf33dc07839049edd5e7f
Karan Pillai <karanpillai93@gmail.com>
2019-03-03 19:22:10 +0530

    Implemented App component
    Added App.js with routing for the application

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// Imports
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./screens/Home/Home";
+import Login from "./screens/Login/Login";
+import Profile from "./screens/Profile/Profile";
+import "./App.css";
+
+// App component
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Login} />
+          {/* <Route exact path="/home" component={Home} /> */}
+          {/* <Route exact path="/profile" component={Profile} /> */}
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+// Export
+export default App;
