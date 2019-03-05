2142318f542743987b0dc16648580f3110f9a9d4
Karan Pillai <karanpillai93@gmail.com>
2019-03-05 14:49:17 +0530

    Uncommented App.js

--- src/App.js
+++ src/App.js
@@ -13,10 +13,9 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div>
-          {/*<Route exact path="/" component={Login} />*/}
-          {/* <Route exact path="/home" component={Home} /> */}
-          {/* <Route exact path="/profile" component={Profile} /> */}
-          <Profile/>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/profile" component={Profile} />
         </div>
       </BrowserRouter>
     );
